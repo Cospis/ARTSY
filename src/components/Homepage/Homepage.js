6d5c79177d94939d8e045bb58c331ccfb7ce8871
Cospis <qoimidris@gmail.com>
2023-01-01 01:25:55 +0100

    Added upcoming without responsive

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,12 +1,14 @@
 import FeaturedProjects from "../FeaturedProducts/FeaturedProduct";
 import DefaultPhotography from "../photography/photography";
 import "./Homepage.css";
+import Upcomingauction from "./../Upcoming/Upcoming";
 
 const DefaultHomepage = () => {
   return (
     <div className="defaulthomepage">
       <DefaultPhotography />
       <FeaturedProjects />
+      <Upcomingauction />
     </div>
   );
 };
--- src/components/Upcoming/Upcoming.js
+++ src/components/Upcoming/Upcoming.js
@@ -1,8 +1,33 @@
 import "./Upcoming.css";
+import bg from "./Imgs/background.png";
 const Upcomingauction = () => {
   return (
     <div className="upcomingauction">
-      <p>Upcoming</p>
+      <h1>See Upcoming Auctions and Exhibitions</h1>
+      <img src={bg} alt="Bg" />
+      <p id="index">01</p>
+      <div className="bgtext">
+        <ul>
+          <li>MONALISA REDEFINED IN STYLE.</li>
+        </ul>
+        <br />
+        <p>Start on : 08:00 GTS . Monday</p>
+        <br />
+        <div className="ptext">
+          <div className="xclus">
+            <p>
+              GET EXCLUSIVE VIEWING OF CONTEMPORARY ART AND CONNECT WITH
+              INVESTORS AND AUCTIONEERS ACROSS THE WORLD BRINGING THEIR HIGHEST
+              AND LOWEST BIDS.
+            </p>
+          </div>
+
+          <div className="ptextseg">
+            <a href="/">See More</a>
+            <button>Set a reminder</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
